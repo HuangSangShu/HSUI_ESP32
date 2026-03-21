f03757bee13fa20b45caa84c79fe81bfa877f610
HuangSangShu <135507833+HuangSangShu@users.noreply.github.com>
2026-03-21 23:27:56 +0800

    2026/03/21 目录建设
    成功地创建了一个只有目录的目录

--- main.py
+++ main.py
@@ -85,7 +85,7 @@ def main():
     fs_drv = lv.fs_drv_t()
     fs_driver.fs_register(fs_drv, 'F')
 
-    global FONT48,FONT14,s_text14,BACK,FRONT
+    global FONT48,FONT14,s_text14,BACK,FRONT,b_boot,b_home
 
     FONT48 = lv.binfont_create('F:/HSUIFONT48.bin')
     FONT14 = lv.binfont_create('F:/HSUIFONT14.bin')
@@ -101,6 +101,9 @@ def main():
     SCR = lv.screen_active()
     scr = lv.obj()
     scr.set_style_bg_color(BACK, 0)
+
+    b_boot = Pin(0,Pin.IN)
+    b_home = Pin(39,Pin.IN)
 
 
     d_logo = lv.label(scr)
@@ -118,7 +121,7 @@ def main():
 
 
 def mainscreen():      
-    global FONT48,FRONT,BACK,s_text14
+    global FONT48,FRONT,BACK,s_text14,b_home,b_boot
 
     scr = lv.obj()
     scr.set_style_bg_color(BACK, 0)
@@ -143,61 +146,92 @@ def mainscreen():
     d_date.add_style(s_text14,0)
     d_date.set_pos(145,180)
 
-    lv.screen_load(scr)
+    lv.screen_load(scr)    
 
     while True:
         now_time = time.localtime()
         d_main_clock.set_text(f"{now_time[3]:02d}:{now_time[4]:02d}:{now_time[5]:02d}")
         d_weekday.set_text(weekdays_chinese[now_time[6]])
         d_date.set_text(f"{now_time[1]}/{now_time[2]}")
-        for i in range(10):
-            if Pin(39,Pin.IN,Pin.PULL_UP) == 0:
-                time.sleep_ms(20)
-                if Pin(39,Pin.IN,Pin.PULL_UP) == 0:
-                    mainmenu()
-                    while Pin(39,Pin.IN,Pin.PULL_UP) == 0:
-                        time.sleep_ms(10)
-            else:
-                time.sleep(0.1)
+        if b_boot.value() == 0:
+            time.sleep_ms(20)
+            if b_boot.value() == 0:
+                mainmenu()
+                while b_boot.value() == 0:
+                    time.sleep_ms(10)
+            
+        time.sleep_ms(50)
 
 
 def mainmenu():
-    global FRONT,BACK,s_text14
+    global FRONT, BACK, s_text14, b_boot, b_home
 
+    # 创建新屏幕，背景色为 BACK
     scr = lv.obj()
     scr.set_style_bg_color(BACK, 0)
 
-    d_menu = lv.menu_create(scr)
+    # 创建菜单对象，并设置为全屏
+    d_menu = lv.menu(scr)
+    d_menu.set_size(lv.pct(100), lv.pct(100))   # 充满屏幕
     d_menu.center()
+
+    # 设置菜单背景色为 BACK（可选，因为屏幕背景已经是 BACK，菜单默认透明）
+    # 如果需要菜单本身也是 BACK，可取消下面一行注释
+    d_menu.set_style_bg_color(BACK, 0)
+
+    # ----- 返回按钮设置 -----
+    back_btn = d_menu.get_main_header_back_button()
+    # 返回按钮上的标签
+    back_label = lv.label(back_btn)
+    back_label.add_style(s_text14, 0)          # 应用文本样式
+    back_label.set_text("返回")
+
+    # ----- 头部标题样式（如果存在）-----
+    header = d_menu.get_main_header()
+    # 头部背景可设置为 BACK 或保持透明
+    header.set_style_bg_color(BACK, 0)       # 可选
+    if header.get_child_count() > 0:
+        title_label = header.get_child(0)      # 通常第一个子对象是标题标签
+        title_label.add_style(s_text14, 0)     # 应用文本样式
+
+    # ----- 创建页面 -----
+    # 设置页面
+    settings_page = lv.menu_page(d_menu, "设置")
     
-    d_back_btn = lv.menu_get_main_header_back_button(d_menu)
-    d_back_button_label = lv.label_create(d_back_btn)
-    d_back_button_label.add_style(s_text14,0)
-    d_back_button_label.set_text("Back")
+    header = d_menu.get_main_header()
+    header.set_style_text_font(FONT14, 0)        # 设置字体
+    header.set_style_text_color(FRONT, 0)  
 
-    d_setting = lv.menu_page_create(d_menu,"设置")
-    cont = lv.menu_cont_create(d_setting)
-    label = lv.label_create(cont)
-    label.set_text("none")
+    # 设置页面内容
+    cont = lv.menu_cont(settings_page)
+    label = lv.label(cont)
+    label.set_text("None")
+    label.add_style(s_text14, 0)               # 应用文本样式
+    label.center()
 
-    d_menu_main_page = lv.menu_page_create(d_menu, None)
+    # 主页面（无标题）
+    main_page = lv.menu_page(d_menu, None)
 
-    cont = lv.menu_cont_create(d_menu_main_page)
-    label = lv.label_create(cont)
-    label.set_text("Item 1 (Click me!)")
-    lv.menu_set_load_page_event(d_menu, cont, d_setting)
-    
-    header = lv.menu_get_main_header(d_menu)
-    title_label = lv.obj_get_child(header, 0)
-    title_label.add_style(s_text14, 0)
+    # 主页面菜单项
+    cont = lv.menu_cont(main_page)
+    label = lv.label(cont)
+    label.set_text("设置")
+    label.add_style(s_text14, 0)               # 应用文本样式
+    d_menu.set_load_page_event(cont, settings_page)
 
+    # 设置初始显示的页面
+    d_menu.set_page(main_page)
+
+    # 加载屏幕
+    lv.screen_load(scr)
+
+    # 等待返回主屏幕的按键（b_boot）
     while True:
-        if Pin(0,Pin.IN,Pin.PULL_UP) == 0:
+        if b_boot.value() == 0:
             time.sleep_ms(20)
-            if Pin(0,Pin.IN,Pin.PULL_UP) == 0:
+            if b_boot.value() == 0:
                 mainscreen()
-                while Pin(0,Pin.IN,Pin.PULL_UP) == 0:
+                while b_boot.value() == 0:
                     time.sleep_ms(10)
-
 
 main()
