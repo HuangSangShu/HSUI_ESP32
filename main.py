97280f99dcdb47880ac35615a8dfa6563119db08
HuangSangShu <135507833+HuangSangShu@users.noreply.github.com>
2026-03-21 20:45:59 +0800

    2026/03/21 主页编写及目录尝试
    为啥按钮按了跳不到mainmenu函数里呢……

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import time
-import machine
-from machine import Pin
-from micropython import const
+import machine 
+from machine import Pin 
+from micropython import const 
+import lcd_bus 
+import fs_driver 
+import lvgl as lv 
 
-import lcd_bus
 
 def main():
     acc = Pin(40, Pin.OUT)
@@ -75,40 +77,127 @@ def main():
     
     display.set_rotation(lv.DISPLAY_ROTATION._270)
   
-    display.set_backlight(100)
+    display.set_backlight(75)
     
-    #出问题就注释以下一行以显示错误报告
-    th = task_handler.TaskHandler()
+    #联机调试时注释以下一行，单机运行请保留该行
+    th = task_handler.TaskHandler()      
+
+    fs_drv = lv.fs_drv_t()
+    fs_driver.fs_register(fs_drv, 'F')
+
+    global FONT48,FONT14,s_text14,BACK,FRONT
+
+    FONT48 = lv.binfont_create('F:/HSUIFONT48.bin')
+    FONT14 = lv.binfont_create('F:/HSUIFONT14.bin')
+
+    FRONT = lv.color_make(100,80,60)
+    BACK = lv.color_make(255,240,200)
+
+    s_text14 = lv.style_t()
+    s_text14.init()
+    s_text14.set_text_font(FONT14)
+    s_text14.set_text_color(FRONT)
+
+    SCR = lv.screen_active()
+    scr = lv.obj()
+    scr.set_style_bg_color(BACK, 0)
+
+
+    d_logo = lv.label(scr)
+    d_logo.add_style(s_text14, 0)
+    d_logo.set_pos(130,100)        
+    d_logo.set_text("HSOS")
+
+    lv.screen_load(scr)
+
+    time.sleep(2)
     
-    scrn = lv.screen_active()
-    scrn.set_style_bg_color(lv.color_hex(0x000000), 0)
-
-    #以上代码感谢Cookie_987提供的支持 | THANKS TO Cookie_987 FOR THE ABOVE CODE
-
-    #----------修改上限----------
-
-    style = lv.style_t()
-    style.init()
-    style.set_text_font(lv.font_montserrat_16)
-
-    time_label = lv.label(scrn)
-    time_label.add_style(style, 0)
-    time_label.set_pos(0,0)
-    time_label.set_text("loading")
-
-    calendar = lv.calendar(scrn)    
-    calendar.set_size(300, 200)
-    calendar.set_pos(10, 25)
-    calendar.set_today_date(2026,3,2)
-    calendar.set_month_shown(2026,3)    
+    mainscreen()
     
-          
+           
+
+
+def mainscreen():      
+    global FONT48,FRONT,BACK,s_text14
+
+    scr = lv.obj()
+    scr.set_style_bg_color(BACK, 0)
+
+    s_main_clock = lv.style_t()
+    s_main_clock.init()
+    s_main_clock.set_text_font(FONT48)
+    s_main_clock.set_text_color(FRONT)
+    
+    
+    d_main_clock = lv.label(scr)
+    d_main_clock.add_style(s_main_clock, 0)
+    d_main_clock.set_pos(40,70)        
+    d_main_clock.set_text("00:00:00")
+
+    d_weekday = lv.label(scr)
+    d_weekday.add_style(s_text14,0)
+    d_weekday.set_pos(40,180)
+    weekdays_chinese = ["星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六"]    
+
+    d_date = lv.label(scr)
+    d_date.add_style(s_text14,0)
+    d_date.set_pos(145,180)
+
+    lv.screen_load(scr)
+
     while True:
         now_time = time.localtime()
-        time_label.set_text(f"{now_time[3]:02d}:{now_time[4]:02d}:{now_time[5]:02d}")
-        time.sleep(1)
+        d_main_clock.set_text(f"{now_time[3]:02d}:{now_time[4]:02d}:{now_time[5]:02d}")
+        d_weekday.set_text(weekdays_chinese[now_time[6]])
+        d_date.set_text(f"{now_time[1]}/{now_time[2]}")
+        for i in range(10):
+            if Pin(39,Pin.IN,Pin.PULL_UP) == 0:
+                time.sleep_ms(20)
+                if Pin(39,Pin.IN,Pin.PULL_UP) == 0:
+                    mainmenu()
+                    while Pin(39,Pin.IN,Pin.PULL_UP) == 0:
+                        time.sleep_ms(10)
+            else:
+                time.sleep(0.1)
 
-if __name__ == '__main__':
-    main()
+
+def mainmenu():
+    global FRONT,BACK,s_text14
+
+    scr = lv.obj()
+    scr.set_style_bg_color(BACK, 0)
+
+    d_menu = lv.menu_create(scr)
+    d_menu.center()
     
+    d_back_btn = lv.menu_get_main_header_back_button(d_menu)
+    d_back_button_label = lv.label_create(d_back_btn)
+    d_back_button_label.add_style(s_text14,0)
+    d_back_button_label.set_text("Back")
 
+    d_setting = lv.menu_page_create(d_menu,"设置")
+    cont = lv.menu_cont_create(d_setting)
+    label = lv.label_create(cont)
+    label.set_text("none")
+
+    d_menu_main_page = lv.menu_page_create(d_menu, None)
+
+    cont = lv.menu_cont_create(d_menu_main_page)
+    label = lv.label_create(cont)
+    label.set_text("Item 1 (Click me!)")
+    lv.menu_set_load_page_event(d_menu, cont, d_setting)
+    
+    header = lv.menu_get_main_header(d_menu)
+    title_label = lv.obj_get_child(header, 0)
+    title_label.add_style(s_text14, 0)
+
+    while True:
+        if Pin(0,Pin.IN,Pin.PULL_UP) == 0:
+            time.sleep_ms(20)
+            if Pin(0,Pin.IN,Pin.PULL_UP) == 0:
+                mainscreen()
+                while Pin(0,Pin.IN,Pin.PULL_UP) == 0:
+                    time.sleep_ms(10)
+
+
+main()
